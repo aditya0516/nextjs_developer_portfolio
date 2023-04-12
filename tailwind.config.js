5c4c9718563c5c20cb9523bc7b2bc8611aea9c10
Aditya <avj22@njit.edu>
2023-04-11 23:22:40 -0400

    added assets and completed setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const {fontFamily} = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -12,6 +13,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
+      }
     },
   },
   plugins: [],
