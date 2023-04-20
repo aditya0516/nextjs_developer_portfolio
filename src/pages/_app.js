49e06d667bfbc957770347c99f84f8c36cc0ba50
aditya0516 <avj22@njit.edu>
2023-04-19 22:53:07 -0400

    minor changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,25 +9,24 @@ const montserrat = Montserrat({
     subsets: ["latin"],
     variable: "--font-mont",
 });
-export default function App({Component, pageProps}) {
+const App = ({Component, pageProps}) => {
     return (
         <>
-            <ErrorBoundary><Head>
+            <Head>
                 <title>Meet Aditya...</title>
                 <link rel="icon" href="/favicon.ico"/>
                 <metadata
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
-            </Head></ErrorBoundary>
-            <ErrorBoundary>
-                <main
-                    className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
-                    <ErrorBoundary><Navbar/></ErrorBoundary>
-                    <ErrorBoundary><Component {...pageProps} /></ErrorBoundary>
-                    <ErrorBoundary><Footer/></ErrorBoundary>
-                </main>
-            </ErrorBoundary>
+            </Head>
+            <main
+                className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+                <ErrorBoundary><Navbar/></ErrorBoundary>
+                <ErrorBoundary><Component {...pageProps} /></ErrorBoundary>
+                <ErrorBoundary><Footer/></ErrorBoundary>
+            </main>
         </>
     );
-}
+};
+export default App;
