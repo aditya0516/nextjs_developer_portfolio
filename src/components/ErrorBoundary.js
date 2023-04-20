7b044741e13b5fdb8a6286c328f21d03c866c228
aditya0516 <avj22@njit.edu>
2023-04-19 22:39:05 -0400

    modified error boundary

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -2,23 +2,33 @@ import React from 'react';
 class ErrorBoundary extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {hasError: false};
-    }
-
-    static getDerivedStateFromError(error) {
-        // Update state so the next render will show the fallback UI.
-        return {hasError: true};
+        this.state = { error: null, errorInfo: null };
     }
 
     componentDidCatch(error, errorInfo) {
-        // You can also log the error to an error reporting service
-        console.log(error, errorInfo);
+        // Catch errors in any components below and re-render with error message
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+        // You can also log error messages to an error reporting service here
     }
 
     render() {
-        if (this.state.hasError) {      // You can render any custom fallback UI
-            return <h1>Something went wrong.</h1>;
+        if (this.state.errorInfo) {
+            // Error path
+            return (
+                <div>
+                    <h2>Something went wrong.</h2>
+                    <details style={{ whiteSpace: 'pre-wrap' }}>
+                        {this.state.error && this.state.error.toString()}
+                        <br />
+                        {this.state.errorInfo.componentStack}
+                    </details>
+                </div>
+            );
         }
+        // Normally, just render children
         return this.props.children;
     }
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,33 @@
 import "@/styles/globals.css";
-import { Montserrat } from "next/font/google";
+import {Montserrat} from "next/font/google";
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import ErrorBoundary from "@/components/ErrorBoundary";
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-mont",
+    subsets: ["latin"],
+    variable: "--font-mont",
 });
-export default function App({ Component, pageProps }) {
-  return (
-    <ErrorBoundary>
-      <Head>
-        <title>Meet Aditya...</title>
-        <link rel='icon' href='/favicon.ico' />
-        <metadata
-          name='viewport'
-          content='width=device-width, initial-scale=1.0'
-        />
-      </Head>
-      <main
-        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
-          <ErrorBoundary><Navbar/></ErrorBoundary>
-          <ErrorBoundary><Component {...pageProps} /></ErrorBoundary>
-          <ErrorBoundary><Footer/></ErrorBoundary>
-      </main>
-    </ErrorBoundary>
-  );
+export default function App({Component, pageProps}) {
+    return (
+        <>
+            <ErrorBoundary><Head>
+                <title>Meet Aditya...</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <metadata
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head></ErrorBoundary>
+            <ErrorBoundary>
+                <main
+                    className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+                    <ErrorBoundary><Navbar/></ErrorBoundary>
+                    <ErrorBoundary><Component {...pageProps} /></ErrorBoundary>
+                    <ErrorBoundary><Footer/></ErrorBoundary>
+                </main>
+            </ErrorBoundary>
+        </>
+    );
 }
