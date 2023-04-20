51dcccc5245bff0648696ce40101514aa35320b5
aditya0516 <avj22@njit.edu>
2023-04-19 22:59:25 -0400

    another try 1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import {Montserrat} from "next/font/google";
-// import Head from "next/head";
+import Head from "next/head";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import ErrorBoundary from "@/components/ErrorBoundary";
@@ -12,14 +12,14 @@ const montserrat = Montserrat({
 const App = ({Component, pageProps}) => {
     return (
         <>
-            {/*<Head>*/}
-            {/*    <title>Meet Aditya...</title>*/}
-            {/*    <link rel="icon" href="/favicon.ico"/>*/}
-            {/*    <metadata*/}
-            {/*        name="viewport"*/}
-            {/*        content="width=device-width, initial-scale=1.0"*/}
-            {/*    />*/}
-            {/*</Head>*/}
+            <Head>
+                <title>Meet Aditya...</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <metadata
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
             <main
                 className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
                 <ErrorBoundary><Navbar/></ErrorBoundary>
