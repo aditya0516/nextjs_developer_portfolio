9e7951b85ef7bb8b73a8f9ba8c6442de03a772e7
aditya0516 <avj22@njit.edu>
2023-04-19 22:20:34 -0400

    added ErrorBoundary to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,11 @@ import Image from "next/image";
 import Link from "next/link";
 import profilePic from "../../public/images/profile/developer-pic-1.png";
 import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function Home() {
   return (
-    <>
+    <ErrorBoundary>
       <Head>
         <title>Meet Aditya...</title>
         <link rel='icon' href='/favicon.ico' />
@@ -67,6 +68,6 @@ export default function Home() {
           />
         </div>
       </main>
-    </>
+    </ErrorBoundary>
   );
 }
